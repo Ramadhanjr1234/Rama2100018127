6f4e832d720f074b93dd58adc259d0986dca1956
Ramadhanjr1234 <ramadhanjr1234@gmail.com>
2022-03-18 09:32:28 +0800

    membuat kalkulator sederhana

---
+++ Kalkulator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float a, b, hasil;
+	char aritmatika;
+	
+	cout<<"PROGRAM KALKULATOR\n \n";
+	cout<<"Menu Operasi : "<<endl;
+	cout<<"  1. Penambahan"<<endl;
+	cout<<"  2. Pengurangan"<<endl;
+	cout<<"  3. Perkalian"<<endl;
+	cout<<"  4. Pembagian"<<endl;
+	cout<<"Pilih Menu No : ";
+	cin>>aritmatika;
+	
+	//memasukkan input dari user
+	cout<<"Masukkan nilai pertama: ";
+	cin>> a;
+	
+	cout<<"Masukkan nilai kedua: ";
+	cin>> b;
+	
+	cout<<"\nHasil perhitungan: ";
+	cout<< a << aritmatika << b;
+	
+	if (aritmatika == '1'){
+		hasil = a + b;
+	    cout<<"Hasil"<<a<<"+"<<b<<"="<<tambah<<(a,b)
+	}else if (aritmatika == '2'){
+		hasil = a - b;
+	}else if (aritmatika == '3'){
+		hasil = a * b;
+	}else if (aritmatika == '4'){
+		hasil = a / b;
+	}else{
+		cout << "Operator anda salah"<<endl;
+	}
+	
+	
+	
+	
+	cin.get();
+	return 0;
+}
